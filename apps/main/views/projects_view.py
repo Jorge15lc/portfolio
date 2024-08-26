1966e056eb6cc439174f1849913d7016a60d4c10
Jorge <jorge15lcbskt@gmail.com>
2024-08-26 18:41:51 +0200

    dwad

--- apps/main/views/projects_view.py
+++ apps/main/views/projects_view.py
@@ -1,5 +1,17 @@
-from django.views.generic import TemplateView
+from typing import Any
+from django.views.generic import ListView
+from apps.main.models import Work, Category
 
-class ProjectsView(TemplateView):
-    template_name = 'main/projects.html'
-    
+class ProjectsView(ListView):
+    template_name = 'projects/projects.html'
+    model = Work
+    paginate_by = 50
+    context_object_name = 'works'
+    ordering = ['-created']
+
+    def get_context_data(self, **kwargs):
+        context  = super().get_context_data(**kwargs)
+        
+        context['categories'] = Category.objects.filter(works__isnull=False).distinct()
+
+        return context 
